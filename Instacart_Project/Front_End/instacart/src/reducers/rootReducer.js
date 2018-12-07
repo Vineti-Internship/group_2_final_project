527e6cd2010d3cae02953cf07287bbd11b098155
Vanik <vaniksarajyan@gmail.com>
2018-12-08 00:59:14 +0400

    create redux boilerplate

---
+++ Instacart_Project/Front_End/instacart/src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    user: {}
+}
+
+export const rootReducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state; 
+    }
+}
