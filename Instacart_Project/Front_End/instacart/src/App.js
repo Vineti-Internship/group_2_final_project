8351bbeb133a689145f4dbfa05af3df41d86baa7
Vanik <vaniksarajyan@gmail.com>
2018-12-08 00:41:09 +0400

    add basic routes for existing components, add Home and Error components

--- Instacart_Project/Front_End/instacart/src/App.js
+++ Instacart_Project/Front_End/instacart/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import { Sign_In } from './components/Sign_In/Sign_in';
 import { Sign_Up } from './components/Sign_Up/Sign_up';
+import Home from './components/Home/Home';
+import Error from './components/Error/Error';
 
 
 class App extends React.Component {
   render() {
-  return <Sign_Up/>
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" exact component={Sign_In} />
+          <Route path="/signup" exact component={Sign_Up}/>
+          <Route component={Error}/>
+        </Switch>
+      </Router>
+    )
   }
 }
 
