d1e7ed796a705c30cde08ecd4130d25fea64d726
danomov <dx555r@gmail.com>
2018-12-08 16:56:35 +0400

    Added extra validations for signUp data

--- Instacart_Project/Back_End/instacart-api/app/models/user.rb
+++ Instacart_Project/Back_End/instacart-api/app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_Name, presence: true
-  validates :last_Name, presence: true
+  validates :first_Name, presence: true, format: { with: /\A[a-zA-Z]+\z|\A[a-zA-Z]+\-+[a-zA-Z]+\z/, message: 'Only allows letters and - character' }
+  validates :last_Name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'Only allows letters' }
   validates :address, presence: true
-  validates :phone, presence: true
-  validates :email, presence: true
+  # format: { with: /\A[a-zA-Z0-9\s]+\z/i, message: 'Only allows letters and numbers' }
+  validates :phone, presence: true, format: { with: /\A\++\d+\z|\A\d+\z/, message: 'Only allows numbers and + character for regional numbers' }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'Invalid Email'}
   validates :password, presence: true
  
 end
