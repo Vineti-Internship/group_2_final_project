ba1f3c38e9437e7a3d7008f48162ff2ce8fded43
Vanik <vaniksarajyan@gmail.com>
2018-12-08 20:27:39 +0400

    add generalFetch helper function

---
+++ Instacart_Project/Front_End/instacart/src/helpers/generalFetch.js
@@ -0,0 +1,18 @@
+const defaultHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  };
+  
+export const generalFetch = async (url, method, body, headers = defaultHeaders) => {
+    try {
+        const response = await fetch(`http://localhost:3000/${url}`,{
+            method,
+            body: JSON.stringify(body),
+            headers
+        });
+        const data = await response.json();
+        return data;
+    } catch(e){
+        console.log('Request failed', e);
+    }
+  }
