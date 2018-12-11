261ce49a0aad0b9700a5076fdcee68b6178edf31
Vanik <vaniksarajyan@gmail.com>
2018-12-11 19:21:38 +0400

    add Stores, add PrivateRoute

---
+++ Instacart_Project/Front_End/instacart/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+ 
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        localStorage.getItem('user')
+            ? <Component {...Component} />
+            : <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+    )} />
+)
+
+export default PrivateRoute;
---
+++ Instacart_Project/Front_End/instacart/src/components/Stores/Stores.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Stores extends React.PureComponent {
+    render(){
+        return (
+            <React.Fragment>
+                <h1>Our Stores</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Stores;
